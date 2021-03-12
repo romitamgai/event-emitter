549f0587d4d1c537a66b9d7eff3890dce065d39d
Romit Amgai <rom_amgai@hotmail.com>
2021-03-12 10:51:04 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const assert = require("assert");
+
+class EventEmitter {
+  constructor() {
+    this.listener = {};
+  }
+
+  on(eventName, fn) {}
+
+  emit(eventName, ...args) {}
+}
+
+const iShouldBeCalled = (args) => {
+  assert(args[0] === 123, 'Args 0 incorrect');
+  assert(args[1] === 456, 'Args 1 incorrect');
+
+  console.log('Completed');
+};
+
+const eventEmitter = new EventEmitter();
+console.log('Started');
